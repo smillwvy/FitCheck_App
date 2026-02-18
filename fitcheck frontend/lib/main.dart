03d5d0aa7e5e0b37712c16f4e162d4cf4885deee
Maheer Basit <maheerbasitahmed@gmail.com>
2026-02-18 16:19:20 +0000

    Update dependencies and app configuration

--- fitcheck frontend/lib/main.dart
+++ fitcheck frontend/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-//import 'Presentation/auth/pages/register_page.dart';
+import 'Presentation/auth/pages/register_page.dart';
 import 'Presentation/app/app_pages/wardrobe_page.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'Presentation/auth/pages/login_page.dart';
@@ -25,9 +25,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const WardrobePage(),
-      // home: const RegisterPage(),
-      // home: const LoginPage(),
+      //home: const WardrobePage(),
+      home: const RegisterPage(),
+      //home: const LoginPage(),
     );
   }
 }
